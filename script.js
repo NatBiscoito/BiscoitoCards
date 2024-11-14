025e2f9c7396d6817bc9ac2c95a01d473e2e28da
NatBiscoito <natsukigu729@gmail.com>
2024-11-13 22:47:49 -0300

    Saving

--- script.js
+++ script.js
@@ -1,22 +1,43 @@
+// Array dos caracteres
 let a = [
-    {nome: "Carro",
-     resposta: "Automovel",},
-    {nome: "Humano",
-     resposta: "Especie",},
-    {nome: "Nat",
-     resposta: "Louca",},
-    {nome: "Cachorro",
-     resposta: "Especie",}
-                          
-];
+     {nome: "Ω",
+     resposta: "Omega"},
+     {nome: "Π",
+     resposta: "Pi"},
+     {nome: "Σ",
+     resposta: "Sigma"},
+     {nome: "Ξ",
+     resposta: "Xi"},
+    ];
 
-let i = Math.floor(Math.random() * a.length);
+let sorteados = []
 
-let r = a[i];
-console.log(r); 
+let botões = [document.getElementById("button1"), document.getElementById("button2"), document.getElementById("button3")];
 
-let answer = prompt(`Um ${r.nome} é:`)
-
-if(answer === r.resposta){
-    alert("Adivinhou!")
+// Função para mudar o texto na tela
+function eText(id, text){
+    let campo = document.getElementById(id);
+    campo.innerHTML = text;
 }
+
+// Função inicial
+function randomGenerator(){
+    let i = Math.floor(Math.random() * a.length);
+    let r = a[i];
+    sorteados.push(r);
+    console.log(sorteados); 
+    eText("middle-char", `${r.nome}`);
+    buttonShuffler(r);
+}
+
+function buttonShuffler(correto) {
+}
+
+function verifier(event){
+    let buttonClicked = event.target.innerHTML;
+    if(buttonClicked === correto){
+        return randomGenerator();
+    }
+} 
+
+randomGenerator()
